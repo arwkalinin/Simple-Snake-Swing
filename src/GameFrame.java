1df6636a563f676cf6ac3224ba11bbf41334c74a
Alexander Kalinin <arw.kalinin@gmail.com>
2022-04-26 04:51:38 +0300

    final

--- src/GameFrame.java
+++ src/GameFrame.java
@@ -4,6 +4,7 @@ public class GameFrame extends JFrame {
     GameFrame() {
         this.add(new GamePanel());
         this.setTitle("Simple Snake Game");
+        this.setIconImage((new ImageIcon("snakeIcon.png")).getImage());
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setResizable(false);
         this.pack();
--- src/GamePanel.java
+++ src/GamePanel.java
@@ -54,11 +54,11 @@ public class GamePanel extends JPanel implements ActionListener {
 
             for (int i = 0; i < snakeSize; i++) {
                 if (i == 0) {
-                    g.setColor(Color.green);
+                    g.setColor(new Color(0, 144, 0));
                     g.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
                 }
                 else {
-                    g.setColor(new Color(45,180,0));
+                    g.setColor(new Color(114, 251, 38));
                     // random snake color (optional)
                     /*
                     g.setColor(new Color(random.nextInt(255),random.nextInt(255),random.nextInt(255)));
