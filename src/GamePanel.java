ac524c2b3f2a0810a659f90698a6ef6f93fc2300
Alexander Kalinin <arw.kalinin@gmail.com>
2022-04-26 03:02:45 +0300

    project structure

---
+++ src/GamePanel.java
@@ -0,0 +1,47 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.Random;
+
+public class GamePanel extends JPanel implements ActionListener {
+    static final int SCREEN_WIDTH = 600;
+    static final int SCREEN_HEIGHT = 600;
+
+    GamePanel() {
+
+    }
+    public void startGame() {
+
+    }
+    public void paintComponent(Graphics g) {
+
+    }
+    public void draw(Graphics g) {
+
+    }
+    public void move() {
+
+    }
+    public void checkApple() {
+
+    }
+    public void checkCollisions() {
+
+    }
+    public void gameOver(Graphics g) {
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+
+    // INNER CLASS
+    public class MyKeyAdapted extends KeyAdapter {
+        @Override
+        public void keyPressed(KeyEvent e) {
+
+        }
+    }
+}
---
+++ src/SnakeGameProcess.java
@@ -0,0 +1,5 @@
+public class SnakeGameProcess {
+    public static void main(String[] args) {
+        new GameFrame();
+    }
+}
